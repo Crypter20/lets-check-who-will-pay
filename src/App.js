54bd921b3bac57fecbd36205dc5dfa0d7bbc35cc
Crypter20 <80840908+Crypter20@users.noreply.github.com>
2021-09-12 18:35:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import { MyContext } from "./context";
+import Stage1 from "./component/stage1";
+import Stage2 from "./component/stage2";
+class App extends React.Component {
+  static contextType = MyContext;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="Wrapper">
+        <div className="center--wrapper">
+          <h1> Who pays the bill? </h1>
+          {this.context.state.stage === 1 ? <Stage1 /> : <Stage2 />}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
